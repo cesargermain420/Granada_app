80cf8ba7f422f3d67d1766de7e3eb90aed4d20fc
Cesar <cesar.germain@gmail.com>
2025-10-02 09:58:04 +0200

    cabecera finalizada

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
 import React from 'react'
 
 export default function App() {
+
+
+  const COLOR_FONDO="#121212"
+  const COLOR_TITULO="#ffdd99"
+  const COLOR_TEXTO_FONDO="#ffffff"
+
+
   return (
-    <View>
-      <Text>App</Text>
+    <View style={[styles.contenedorPrincipal,{backgroundColor:COLOR_FONDO}]}>
+    <ScrollView>
+   <Image 
+    source={require("./assets/fotos/granada_dark.jpg")} 
+    style={{ width: '100%', height: 250 }}
+    />
+
+     </ScrollView>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+contenedorPrincipal:{
+ flex:1,
+ flexDirection:"column"
+}
+
+
+
+
+})
